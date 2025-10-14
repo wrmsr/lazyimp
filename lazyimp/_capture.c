0662b8f8cbc366798435c6ba02b8024fa209eac5
wrmsr <timwilloney@gmail.com>
2025-10-14 11:54:03 -0700

    its Py_MOD_PER_INTERPRETER_GIL_SUPPORTED

--- lazyimp/_capture.c
+++ lazyimp/_capture.c
@@ -77,7 +77,7 @@ static struct PyModuleDef_Slot capture_slots[] = {
     {Py_mod_gil, Py_MOD_GIL_NOT_USED},
 #endif
 #if PY_VERSION_HEX >= 0x030C0000
-    {Py_mod_multiple_interpreters, Py_MOD_MULTIPLE_INTERPRETERS_SUPPORTED},
+    {Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED},
 #endif
     {0, NULL}
 };
